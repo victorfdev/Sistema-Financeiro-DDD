31b7d366403cf1212fc45db0cb2547ebf33d9363
Victor <victor.fsilva06@gmail.com>
2024-03-24 18:52:29 -0300

    :tada: First commit

---
+++ Finaneiro_24/Entities/Notificacoes/Notifica.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Notificacoes
+{
+    public class Notifica
+    {
+        public Notifica()
+        {
+
+            notificacoes = new List<Notifica>();
+
+        }
+
+        [NotMapped]
+        public string NomePropriedade { get; set; }
+
+        [NotMapped]
+        public string Mensagem { get; set; }
+
+        [NotMapped]
+        public List<Notifica> notificacoes;
+
+        public bool ValidarPropriedadeString(String valor, string nomePropriedade)
+        {
+            if (string.IsNullOrWhiteSpace(valor) || string.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                notificacoes.Add(new Notifica
+                {
+                    Mensagem = "Campo Obrigatório",
+                    NomePropriedade = nomePropriedade
+                });
+
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool ValidarPropriedadeInt(int valor, string nomePropriedade)
+        {
+            if(valor < 1 || string.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                notificacoes.Add(new Notifica
+                {
+                    Mensagem = "Campo Obrigatório",
+                    NomePropriedade = "nomePropriedade"
+
+                });
+
+                return false;
+
+            }
+
+            return true;
+        }
+
+    }
+}
